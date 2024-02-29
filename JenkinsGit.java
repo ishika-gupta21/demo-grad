1ca9eecb0981f6aeaf3ec80ec5404a30cd3c3d73
Ishika Gupta <88900614+ishika-gupta21@users.noreply.github.com>
2024-02-29 10:40:18 +0530

    Update JenkinsGit.java
    updated file

--- JenkinsGit.java
+++ JenkinsGit.java
@@ -2,6 +2,6 @@ public class JenkinsGit
 {
 	public static void main(String args[])
 	{
-		System.out.println("Hello Everybody!!!");
+		System.out.println("Hello Everybody!!! Have a nice day :)");
 	}
 }
